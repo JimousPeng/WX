79e356943ca2c1eef41aeeb9d99000fa992176a3
JimousPeng <972523881@qq.com>
2018-12-11 09:50:04 +0800

    测试分支

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div class="header">
-      <input type="text">
+      <i class="iconfont icon-sousuo"></i>
+      <input
+        type="text"
+        placeholder="搜索"
+      >
     </div>
   </div>
 </template>
@@ -9,6 +13,17 @@
 <script>
 </script>
 
-<style>
-
+<style lang="scss">
+$maincolor: #ff2d4a;
+.header {
+  background-color: $maincolor;
+  padding: 20rpx 16rpx;
+  input {
+    height: 100%;
+    height: 60rpx;
+    background-color: white;
+    border-radius: 10rpx;
+    text-align: center;
+  }
+}
 </style>
